a8d9ca362ab2db0dd06b8c64afd51d87c4964cc4
polchky <polchky@gmail.com>
2019-04-08 20:35:00 +0200

    Remove test comments

--- routes/books.js
+++ routes/books.js
@@ -1,34 +1,6 @@
 const books = require('../controllers/books');
 const jwt = require('../middlewares/jwt');
-/**
- * @swagger
- * resourcePath: /api
- * description: All about API
- */
- 
-/**
- * @swagger
- * path: /login
- * operations:
- *   -  httpMethod: POST
- *      summary: Login with username and password
- *      notes: Returns a user based on username
- *      responseClass: User
- *      nickname: login
- *      consumes:
- *        - text/html
- *      parameters:
- *        - name: username
- *          description: Your username
- *          paramType: query
- *          required: true
- *          dataType: string
- *        - name: password
- *          description: Your password
- *          paramType: query
- *          required: true
- *          dataType: string
- */
+
 module.exports = (router) => {
     router
         .param('book_id', books.getById)
