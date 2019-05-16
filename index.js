d96965bd7ce7de14d8a36aeb9fa5ddd6c6bb54c8
Pascal Gremaud <pascal.gremaud@unifr.ch>
2019-05-16 13:23:35 +0200

    Create level 2 app

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 // Create the Koa app
 const app = new Koa();
+const router = new Router();
+
+const users = [
+    { name: 'Pascal', email: 'pascal.gremaud@unifr.ch' },
+    { name: 'Arnaud', email: 'arnaud.durand@unifr.ch' },
+    { name: 'Jacques', email: 'jacques.pasquier@unifr.ch' },
+];
+
+const guard = (ctx, next) => {
+    if (ctx.request.headers.authorization === 'letmein') {
+        return next();
+    } else{
+        ctx.body = 'nope';
+    }
+};
+
+router.get('/', (ctx) => {
+    ctx.body = 'Hello world!';
+});
+
+router.get('/users/', (ctx) => {
+    ctx.body = users;
+});
+
+router.get('/users/:userId', (ctx) => {
+    ctx.body = users[ctx.params.userId];
+});
 
 app
-    .use((ctx) => {
-        console.log(`path: ${ctx.request.path}\n`);
-        console.log(`query: ${ctx.request.querystring}\n`);
-        console.log('headers:');
-        const keys = Object.keys(ctx.request.headers);
-        for (let i = 0; i < keys.length; i += 1) {
-            console.log(`${keys[i]}: ${ctx.request.headers[keys[i]]}`);
-        }
-        console.log('');
-        ctx.body = 'Hello world!';
-    })
+    .use(guard)
+    .use(router.routes())
     .listen(3000);
