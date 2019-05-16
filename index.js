efc8cca6993afacebf6cd935d8e765cc4d5cc789
Pascal Gremaud <pascal.gremaud@unifr.ch>
2019-05-16 11:58:03 +0200

    Add request inspector

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = new Koa();
 
 app
     .use((ctx) => {
+        console.log(`path: ${ctx.request.path}\n`);
+        console.log(`query: ${ctx.request.querystring}\n`);
+        console.log('headers:');
+        const keys = Object.keys(ctx.request.headers);
+        for (let i = 0; i < keys.length; i += 1) {
+            console.log(`${keys[i]}: ${ctx.request.headers[keys[i]]}`);
+        }
+        console.log('');
         ctx.body = 'Hello world!';
     })
     .listen(3000);
